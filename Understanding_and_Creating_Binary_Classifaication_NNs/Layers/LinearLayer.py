41e7c2fcae3bb30622a996c4f8f9c303d6d54610
Rafay Khan <rafayak16@gmail.com>
2019-11-11 14:19:58 +0500

    add Layers directory for Binary Classification

---
+++ Understanding_and_Creating_Binary_Classifaication_NNs/Layers/LinearLayer.py
@@ -0,0 +1,81 @@
+"""
+Created by : Rafay Khan
+Twitter: @RafayAK
+"""
+
+
+import numpy as np  # import numpy library
+from util.paramInitializer import initialize_parameters  # import function to initialize weights and biases
+
+
+class LinearLayer:
+    """
+        This Class implements all functions to be executed by a linear layer
+        in a computational graph
+
+        Args:
+            input_shape: input shape of Data/Activations
+            n_out: number of neurons in layer
+            ini_type: initialization type for weight parameters, default is "plain"
+                      Opitons are: plain, xavier and he
+
+        Methods:
+            forward(A_prev)
+            backward(upstream_grad)
+            update_params(learning_rate)
+
+    """
+
+    def __init__(self, input_shape, n_out, ini_type="plain"):
+        """
+        The constructor of the LinearLayer takes the following parameters
+
+        Args:
+            input_shape: input shape of Data/Activations
+            n_out: number of neurons in layer
+            ini_type: initialization type for weight parameters, default is "plain"
+        """
+
+        self.m = input_shape[1]  # number of examples in training data
+        # `params` store weights and bias in a python dictionary
+        self.params = initialize_parameters(input_shape[0], n_out, ini_type)  # initialize weights and bias
+        self.Z = np.zeros((self.params['W'].shape[0], input_shape[1]))  # create space for resultant Z output
+
+    def forward(self, A_prev):
+        """
+        This function performs the forwards propagation using activations from previous layer
+
+        Args:
+            A_prev:  Activations/Input Data coming into the layer from previous layer
+        """
+
+        self.A_prev = A_prev  # store the Activations/Training Data coming in
+        self.Z = np.dot(self.params['W'], self.A_prev) + self.params['b']  # compute the linear function
+
+    def backward(self, upstream_grad):
+        """
+        This function performs the back propagation using upstream gradients
+
+        Args:
+            upstream_grad: gradient coming in from the upper layer to couple with local gradient
+        """
+
+        # derivative of Cost w.r.t W
+        self.dW = np.dot(upstream_grad, self.A_prev.T)
+
+        # derivative of Cost w.r.t b, sum across rows
+        self.db = np.sum(upstream_grad, axis=1, keepdims=True)
+
+        # derivative of Cost w.r.t A_prev
+        self.dA_prev = np.dot(self.params['W'].T, upstream_grad)
+
+    def update_params(self, learning_rate=0.1):
+        """
+        This function performs the gradient descent update
+
+        Args:
+            learning_rate: learning rate hyper-param for gradient descent, default 0.1
+        """
+
+        self.params['W'] = self.params['W'] - learning_rate * self.dW  # update weights
+        self.params['b'] = self.params['b'] - learning_rate * self.db  # update bias(es)
